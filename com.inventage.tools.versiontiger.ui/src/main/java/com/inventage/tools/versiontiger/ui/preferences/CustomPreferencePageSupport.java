40d34cce6d4e2366bdf29770e7e2286e7f230881
Beat Strasser <beat.strasser@inventage.com>
2013-02-11 15:57:56 +0100

    Fixed #17: Unable to set Release qualifier in preferences

--- com.inventage.tools.versiontiger.ui/src/main/java/com/inventage/tools/versiontiger/ui/preferences/CustomPreferencePageSupport.java
+++ com.inventage.tools.versiontiger.ui/src/main/java/com/inventage/tools/versiontiger/ui/preferences/CustomPreferencePageSupport.java
@@ -3,21 +3,39 @@ package com.inventage.tools.versiontiger.ui.preferences;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.databinding.dialog.DialogPageSupport;
-import org.eclipse.jface.dialogs.DialogPage;
 import org.eclipse.jface.preference.PreferencePage;
 
+/**
+ * Replacement class for {@code PreferencePageSupport} which is buggy in Galileo
+ * (preferencePage is not fetched with getDialogPage(): causes a NPE).
+ */
 public class CustomPreferencePageSupport extends DialogPageSupport {
 
-	protected CustomPreferencePageSupport(DialogPage dialogPage, DataBindingContext dbc) {
-		super(dialogPage, dbc);
+	public CustomPreferencePageSupport(PreferencePage preferencePage, DataBindingContext dbc) {
+		super(preferencePage, dbc);
 	}
 
 	@Override
 	protected void handleStatusChanged() {
 		super.handleStatusChanged();
 		
-		if (currentStatus != null) {
-			((PreferencePage) getDialogPage()).setValid(!currentStatus.matches(IStatus.ERROR | IStatus.CANCEL));
-		}
+		getDialogPage().setValid(isNotStale() && hasValidStatus());
 	}
+
+	@Override
+	protected PreferencePage getDialogPage() {
+		return (PreferencePage) super.getDialogPage();
+	}
+	
+	private boolean isNotStale() {
+		return !currentStatusStale;
+	}
+	
+	private boolean hasValidStatus() {
+		if (currentStatus != null) {
+			return !currentStatus.matches(IStatus.ERROR | IStatus.CANCEL);
+		}
+		return true;
+	}
+
 }
