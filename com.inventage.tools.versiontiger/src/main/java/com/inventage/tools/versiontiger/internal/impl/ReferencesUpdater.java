129c94a2c81349582f9e21b23064a6ed81c9b0c6
Oliver Libutzki <oliver@libutzki.de>
2015-12-07 09:46:54 +0100

    #38: Removed isVersionInherited() check
    Signed-off-by: Oliver Libutzki <oliver@libutzki.de>

--- com.inventage.tools.versiontiger/src/main/java/com/inventage/tools/versiontiger/internal/impl/ReferencesUpdater.java
+++ com.inventage.tools.versiontiger/src/main/java/com/inventage/tools/versiontiger/internal/impl/ReferencesUpdater.java
@@ -20,11 +20,7 @@ class ReferencesUpdater implements Project {
 	}
 
 	public void setVersion(MavenVersion newVersion) {
-		if (isVersionInherited()) {
-			logWarning("Cannot set version for project that heirs version from parent.");
-			return;
-		}
-		
+
 		MavenVersion oldVersion = getVersion();
 
 		project.setVersion(newVersion);
